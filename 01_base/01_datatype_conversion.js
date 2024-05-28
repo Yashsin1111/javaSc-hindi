cc8d40388d7dfb23acab0fd72861fee07c017b7a
Yash Kumar Singh <167507270+Yashsin1111@users.noreply.github.com>
2024-05-28 12:16:19 +0000

    operators and type conversion

--- 01_base/01_datatype_conversion.js
+++ 01_base/01_datatype_conversion.js
@@ -37,3 +37,41 @@ let somenumber=98;
 let thisnumber=String(somenumber);
 console.log(typeof thisnumber);
 console.log(thisnumber);
+
+
+/* operation on javascript */
+
+let value=10;
+let negativevalue= -value;
+// console.log(negativevalue);
+//as like many operation we have 
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(5/2);
+console.log(4%2);
+
+//lets add string 
+let str1="yash";
+let str2=" singh";
+console.log(str1+str2);//we can do only addition operation in string;
+
+console.log("1"+2);//string and number both concluded together
+console.log(1+"2");//number and string ...
+console.log("1"+"2"+3);//string , string and number concluded together
+console.log(1+2+"3");/*but in this case number,number and string ..
+comes together the number add automatacally and string part conclude automatically*/
+
+
+/*general purpose 
+console.log(true),return true
+console.log(+true),return 1
+console.log(true+),return error
+
+let num1,num2,num3
+num1=num2=num3=2+3; not a profesional way to declare it
+*/
+
+let gamecount=1002;
+gamecount++;//as we know there is pre-fix and postfix operation on javascript;
+console.log(gamecount);
