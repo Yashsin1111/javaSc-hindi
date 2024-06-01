70493dc45e6e9473fc0176dd9df58451a7a604de
Yash Kumar Singh <167507270+Yashsin1111@users.noreply.github.com>
2024-06-01 10:12:16 +0000

    objects

---
+++ 02_base/02_object.js
@@ -0,0 +1,42 @@
+//singleton-->when ever we declare an object literals then singleton not formed.
+//object.create, use to create singleton
+
+//object literals
+
+const mysmb=Symbol("mykey1");//we are declare a symbol and we want to use this as a symbol
+
+const user={
+    name:"yash kumar singh", //js takes key as string eg:name="name";
+    age:21,
+    [mysmb]:"mykey",//declare mysmb in square braces,to use as a symbol
+    address:"kalpana colony thikampur ballia",
+    email:"syash7157@gmail.com",
+    isLoggedIn:"false",
+    LastLogged:["monday","tuesday"],
+}
+console.log(user.email);//we can declare by using dot(.),but use [" "],is an efficient way to display it.
+console.log(user["email"]);
+// console.log(user.mysmb);
+// console.log(typeof user.mysmb);//return string 
+// console.log(typeof user[mysmb]);// act as symbol
+// console.log(user);//act as a symobol by removing square brackets it act like a normal string.
+
+user.email="yash886577@gmail.com";//here chnages are preformed
+// Object.freeze(user);//we can freeze the object no change are preformed
+// console.log(user);
+
+user.greeting=function(){
+    console.log("hellow my name is yash");
+}
+// console.log(user.greeting);//function reference,we will discuss it later
+console.log(user.greeting());
+
+user.greeting2=function(){
+    console.log(`hellow my name is,${this.name}`);//here we are converting string into backits called string interpolation
+}
+console.log(user.greeting2());
+
+/*note-
+casully we are accesing the object to (.),but when symbol are defined we have to used square braces if not symbols are treated
+as strings and also js take key as string, and what if key defined as "name": insted of name: so in this case we can't access 
+the object to (.), we have to use square braces.
