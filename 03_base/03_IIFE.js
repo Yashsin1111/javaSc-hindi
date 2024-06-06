9ff385924af3acb83f0ea639221c169816e48360
Yash Kumar Singh <167507270+Yashsin1111@users.noreply.github.com>
2024-06-06 09:50:03 +0000

    IIFE

--- 03_base/03_IIFE.js
+++ 03_base/03_IIFE.js
@@ -5,8 +5,25 @@ Encapsulation: IIFE creates a private scope for variables, preventing them from 
 conflicts with other parts of your code.
 Anonymous or Named: You can define IIFE as an anonymous function (without a name) or a named function (with a name).
 Anonymous IIFE is more common, but named IIFE can be helpful for debugging. */
-(function chai(){
-    console.log("TO-BE FINISHED")
-})()
 /* syntax- ()(),
 [(function defination)(function call)] */
+(function chai(){
+    console.log("TO-BE FINISHED")
+})();//termination is need , because the IIFE do not know were the function is end , if termination is not allowed then 
+// it return error.
+
+(function aurcode(){
+    console.log("To-be-discontineued");
+})();
+
+// we can also do this
+
+( ()=>{
+    console.log("get lost");
+})();
+
+
+( (Name)=>{
+    console.log(`my name is ${Name}`);
+})("hitesh")
+
