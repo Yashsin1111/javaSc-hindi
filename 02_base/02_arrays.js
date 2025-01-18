472f7888232e7f4d4c94ebbcc8c94e084edd4830
Yash Kumar Singh <167507270+Yashsin1111@users.noreply.github.com>
2025-01-18 17:36:56 +0530

    Update 02_arrays.js

--- 02_base/02_arrays.js
+++ 02_base/02_arrays.js
@@ -1,35 +1,42 @@
 //arrays
 
-const arr1=[1,2,3,4,5];
+const arr1=[1,2,3,4,5]; js arrays are dynamic
 console.log(arr1);//array is the type of object
 console.log(typeof arr1);
+
 const arr2=[0,1,2,3,"Mahadev","shree Ram"];//we can use multiple type of datatype in single array.
 console.log(arr2);
 //accessing array elements
 console.log(arr2[4]);
 //we can also declare the arrays in this way
+
 const myarr=new Array(1,2,3,4,5);
 console.log(myarr);
 console.log(myarr[4]);
 //we have some useful methods of arrays
+
 myarr.push(6);
 myarr.push(7);
 console.log(myarr);
+   
 myarr.pop();
 myarr.pop();
 console.log(myarr);
+
 myarr.unshift(9);//adds the element in starting index of the arrays.
 console.log(myarr);
+
 myarr.shift();//delete the starting element(index) of the arrays.
 console.log(myarr);
+
 console.log(myarr.includes(2));//return true or false  
 console.log(myarr.indexOf(5));//return the index of element
 
 const newArry=myarr.join();// its binds the array and convert into string
 
-console.log(myarr);
-console.log(typeof newArry);
-console.log(newArry);
+console.log(myarr); 
+console.log(typeof myarr);  //object
+console.log(typeof newArry);  //string
 
 //splice and slice 
 console.log("original array",myarr);
@@ -43,3 +50,4 @@ console.log(myarray);
 const myarray2=myarr.splice(1,3);
 console.log("splice",myarr);
 console.log(myarray2);
+// splice used to manupulate the original arrays but slice did't.
